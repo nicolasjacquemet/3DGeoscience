1b02f5d028c77c939d1f69d2d5d7e8ba8541184c
nicolasjacquemet <nicolas.jacquemet@recherche.univ-lyon1.fr>
2015-12-03 14:28:51 +0100

    no message

--- 3DGeosciences/Assets/Scripts/OrbitCamera.cs
+++ 3DGeosciences/Assets/Scripts/OrbitCamera.cs
@@ -2,9 +2,35 @@
 using System.Collections;
 
 public class OrbitCamera : MonoBehaviour {
+
+	public float _distance = 2f;
+	public float _sensitivity = 10f;
+
+	float _currentRotationRight = 0f;
+	float _currentRotationUp = 0f; 
+
+	//public float _x = 0f;
+	//public float _y = 0f;
 	
+	void Start(){
+		Rotate (_currentRotationRight, _currentRotationUp);
+	}
+
+
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetMouseButton(0)){
+			_currentRotationRight += Input.GetAxis("Mouse Y") * _sensitivity;
+			_currentRotationUp += Input.GetAxis("Mouse X") * _sensitivity;
+		}
+		Rotate (_currentRotationRight, _currentRotationUp);
+	}
+
+	void Rotate(float degreesAroundRight, float degreesAroundUp){
+		Quaternion rotation = Quaternion.Euler (-degreesAroundRight, degreesAroundUp, 0f);
+		transform.rotation = rotation;
+		Vector3 percheASelfie = new Vector3(0f, 0f, -_distance);
+		transform.position = rotation * percheASelfie;
 		
 	}
 }
