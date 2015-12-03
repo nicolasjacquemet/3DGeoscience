a977fda3a8569886c38b334ebcea1ca9c0f6ca06
nicolasjacquemet <nicolas.jacquemet@recherche.univ-lyon1.fr>
2015-12-03 11:56:20 +0100

    no message

---
+++ 3DGeosciences/Assets/Scripts/Rotater.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour {
+
+	public float speed = 10f;
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up * speed * Time.deltaTime);
+	}
+}
