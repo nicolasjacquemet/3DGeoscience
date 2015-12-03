d3fbd1428804eb39a8fc1311144e4a65863cfb25
nicolasjacquemet <nicolas.jacquemet@recherche.univ-lyon1.fr>
2015-12-03 11:35:11 +0100

    maj scene2

---
+++ 3DGeosciences/Assets/Scripts/SetButtonAlpha.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SetButtonAlpha : MonoBehaviour {
+
+	public void SetAlpha(float alpha){
+		Image image = GetComponent<Image>();
+		Color newColor = image.color;
+		newColor.a = alpha;
+		image.color = newColor;
+	}
+
+}
